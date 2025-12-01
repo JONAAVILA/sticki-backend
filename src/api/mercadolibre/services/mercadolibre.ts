8ada1488ff5cde563c614676747763428f2b7aac
JONAAVILA <jona_1969@hotmail.com>
2025-11-30 22:55:36 -0300

    update buildAuth

--- src/api/mercadolibre/services/mercadolibre.ts
+++ src/api/mercadolibre/services/mercadolibre.ts
@@ -8,15 +8,18 @@ const { CLIENT_ID, CLIENT_SECRET, AUTH_URL, REDIRECT_URI } = process.env
 
 export default factories.createCoreService('api::mercadolibre.mercadolibre',({strapi})=>({
     async buildAuth(){
+        type DataResponse  = {
+            access_token:string
+        }
         const URL = 'https://api.mercadolibre.com/oauth/token'
 
-        const data = {
+        const credentials = {
             client_id:CLIENT_ID,
             client_secret:CLIENT_SECRET,
             grant_type: "client_credentials"
         }
 
-        const dataParse = JSON.stringify(data)
+        const credentialsParse = JSON.stringify(credentials)
 
         try {
             const res = await fetch(URL,{
@@ -24,16 +27,17 @@ export default factories.createCoreService('api::mercadolibre.mercadolibre',({st
                 headers:{
                     "Content-Type": "application/json",
                 },
-                body:dataParse
+                body:credentialsParse
             })
 
-            const data = await res.json()
+            const data = await res.json() as DataResponse
+            const { access_token } = data
 
-            const response = await fetch('https://api.mercadolibre.com/items/MLA1398714241/shipping_options?zip_code=1843y',{
+            const response = await fetch('https://api.mercadolibre.com/items/MLA1398714241/shipping_options?zip_code=1882',{
                 method:'GET',
                 headers:{
                     "Content-Type": "application/json",
-                    Authorization: `Bearer APP_USR-8432012284158585-112910-6d92d21b73a86e7b6db85d795f468164-74051616`,
+                    Authorization: `Bearer ${access_token}`,
                 }
             })
 
