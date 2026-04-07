09db2a2e602870b6af03001ff00a190a443c7871
sidonay499 <jona_1969@hotmail.com>
2026-04-07 18:49:23 -0300

    payment validation added to notification mp

--- src/api/notification/services/notification.ts
+++ src/api/notification/services/notification.ts
@@ -6,6 +6,8 @@ import { factories } from '@strapi/strapi';
 import { mercadopago } from '../../../utils/mpConfig';
 import { Payment } from 'mercadopago';
 
+const { TEST_MODE } = process.env
+
 export default factories.createCoreService('api::notification.notification',({strapi})=>({
     async checkAndUpdateOrder(id : string){
         try {
@@ -13,15 +15,22 @@ export default factories.createCoreService('api::notification.notification',({st
             console.log("payment",payment)
             const paymentId = payment.additional_info.items[0].id
 
-            await strapi.documents('api::order.order').update({
-                documentId:paymentId,
-                locale:'en',
-                data:{
-                    statusOrder:'approved',
-                    payment_id:id
-                },
-                status: 'published',
-            })
+            const isValidPayment =
+                payment.live_mode === true &&
+                payment.status === "aprovved" &&
+                TEST_MODE === "true" ? true : !payment.payer.email.includes("testuser.com")
+
+            if(isValidPayment){
+                await strapi.documents('api::order.order').update({
+                    documentId:paymentId,
+                    locale:'en',
+                    data:{
+                        statusOrder:'approved',
+                        payment_id:id
+                    },
+                    status: 'published',
+                })
+            }
         } catch (error) {
             return strapi.log.error
         }
