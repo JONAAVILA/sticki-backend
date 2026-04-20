1cc5ed478de520540e8473c069913956f82c9a9a
sidonay499 <jona_1969@hotmail.com>
2026-04-20 18:50:29 -0300

    update auth clerk

--- src/middleware/auth-clerk.ts
+++ src/middleware/auth-clerk.ts
@@ -1,42 +1,42 @@
 import { verifyToken } from "@clerk/backend"
 
-export default ({strapi})=>{
-    return async (ctx,next)=>{
-        console.log("ejecuto middle")
-        const authHeader = ctx.request.header.authorization
-        console.log("authheader",authHeader)
-        
-        if(!authHeader || !authHeader.startsWith('Bearer ')){
-            return await next()
-        }
-        
-        const token = authHeader.split(' ')[1]
-        console.log("auth token",token)
+module.exports = (config, { strapi }) => {
+  return async (ctx, next) => {
+    console.log("ejecuto middle")
 
-        try {
-            const payload = await verifyToken(
-                token,
-                {
-                    secretKey:process.env.CLERK_SECRET_KEY
-                }
-            )
+    const authHeader = ctx.request.header.authorization
 
-            if(payload){
-                const clerkId = payload.sub
-                const [user] = await strapi.entityService.findMany('plugin::users-permissions.user', {
-                    filters: { clerkId: clerkId },
-                });
-
-                if(user){
-                    ctx.state.user = user
-                    ctx.isAuthenticated = true
-                }
-                console.log("user middleware",user)
-            }
-        } catch (error) {
-            console.error('Error de validación en Clerk:', error.message)
-            return ctx.unauthorized('Token inválido')
-        }
-        return await next()
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return await next()
     }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+      const payload = await verifyToken(token, {
+        secretKey: process.env.CLERK_SECRET_KEY,
+      })
+
+      if (payload) {
+        const clerkId = payload.sub
+
+        const [user] = await strapi.entityService.findMany(
+          'plugin::users-permissions.user',
+          {
+            filters: { clerkId },
+          }
+        )
+
+        if (user) {
+          ctx.state.user = user
+          ctx.isAuthenticated = true
+        }
+      }
+
+      return await next()
+    } catch (error) {
+      console.error('Error de validación en Clerk:', error.message)
+      return ctx.unauthorized('Token inválido')
+    }
+  }
 }
