e95e581474926866e2e03876a9e3f9b228eb1406
sidonay499 <jona_1969@hotmail.com>
2026-04-20 16:29:35 -0300

    added tray catch to webhook create user

--- src/api/auth/signin/controllers/auth-signin-webhook.ts
+++ src/api/auth/signin/controllers/auth-signin-webhook.ts
@@ -5,51 +5,51 @@ const { CLERK_SECRET_KEY } = process.env
 
 export default {
     async register(ctx:Context){
-        const headers = ctx.request.headers
-        const payload = JSON.stringify(ctx.request.body)
-        const wh = new Webhook(CLERK_SECRET_KEY)
-
-        const svix_id = headers["svix-id"];
-        const svix_timestamp = headers["svix-timestamp"];
-        const svix_signature = headers["svix-signature"];
-
-        if (!svix_id || !svix_timestamp || !svix_signature || Array.isArray(svix_id) || Array.isArray(svix_timestamp) || Array.isArray(svix_signature)) {
-            return ctx.badRequest('Faltan encabezados de Svix o el formato es inválido');
-        }
-
-        let evt
         try {
-            evt = wh.verify(
-                payload,
-                {
-                    "svix-id": svix_id,
-                    "svix-timestamp": svix_timestamp,
-                    "svix-signature": svix_signature,
-                }
-            )
-        } catch (error) {
-            return ctx.badRequest('Firma de Webhook inválida')
-        }
-
-        const { id, image_url, email_addresses, first_name, last_name, external_accounts } = evt.data
-
-        console.log("data webhook", id, image_url, email_addresses, first_name, last_name, external_accounts )
-        
-        const provider = external_accounts.length && external_accounts[0].provider 
-        const email = email_addresses[0].email_address
-        const userName = email.split("@")[0]
-        const type = evt.type
-
-        if(type === "session.created"){
-            const role = await strapi
-                .query("plugin::users-permissions.role")
-                .findOne({ where: { type: "authenticated" } })
-
-            if(!role) {
-                throw new Error("No se encontró el rol 'authenticated' en Strapi");
+            const headers = ctx.request.headers
+            const payload = JSON.stringify(ctx.request.body)
+            const wh = new Webhook(CLERK_SECRET_KEY)
+    
+            const svix_id = headers["svix-id"];
+            const svix_timestamp = headers["svix-timestamp"];
+            const svix_signature = headers["svix-signature"];
+    
+            if (!svix_id || !svix_timestamp || !svix_signature || Array.isArray(svix_id) || Array.isArray(svix_timestamp) || Array.isArray(svix_signature)) {
+                return ctx.badRequest('Faltan encabezados de Svix o el formato es inválido');
             }
-            
+    
+            let evt
             try {
+                evt = wh.verify(
+                    payload,
+                    {
+                        "svix-id": svix_id,
+                        "svix-timestamp": svix_timestamp,
+                        "svix-signature": svix_signature,
+                    }
+                )
+            } catch (error) {
+                return ctx.badRequest('Firma de Webhook inválida')
+            }
+    
+            const { id, image_url, email_addresses, first_name, last_name, external_accounts } = evt.data
+    
+            console.log("data webhook", id, image_url, email_addresses, first_name, last_name, external_accounts )
+            
+            const provider = external_accounts.length && external_accounts[0].provider 
+            const email = email_addresses[0].email_address
+            const userName = email.split("@")[0]
+            const type = evt.type
+    
+            if(type === "session.created"){
+                const role = await strapi
+                    .query("plugin::users-permissions.role")
+                    .findOne({ where: { type: "authenticated" } })
+    
+                if(!role) {
+                    throw new Error("No se encontró el rol 'authenticated' en Strapi");
+                }
+                
                 const user = await strapi
                 .plugin("users-permissions")
                 .service("user")
@@ -68,12 +68,13 @@ export default {
                 console.log("create user",user)
                 
                 return ctx.send({ message: 'Usuario creado' });
-                
-            } catch (error) {
-                console.log("error webhook",error)
+            }else{
+                return console.log("método no autorizado")
             }
             
+        } catch (error) {
+            console.log("error webhook",error)
+            return ctx.send({ message: 'Evento ignorado' });
         }
-        return ctx.send({ message: 'Evento ignorado' });
     }
 }
