4dee57ccdc2944c16df7974d940d2d211860c1b9
JONAAVILA <jona_1969@hotmail.com>
2026-04-21 01:17:42 -0300

    refactorice signup controller

--- src/api/signup/controllers/signup.ts
+++ src/api/signup/controllers/signup.ts
@@ -11,18 +11,12 @@ export default factories.createCoreController('api::signup.signup',({strapi})=>(
     async receive(ctx){
         try {
             const headers = ctx.request.headers
-            console.log("header",headers)
             const body = JSON.stringify(ctx.request.body)
-            console.log("body",body)
             const wh = new Webhook(CLERK_WEBHOOK_SECRET)
-            console.log("webhook",wh)
             
             const svix_id = headers["svix-id"];
-            console.log("svix_id",svix_id)
             const svix_timestamp = headers["svix-timestamp"];
-            console.log("svix_timestamp",svix_timestamp)
             const svix_signature = headers["svix-signature"];
-            console.log("svix_signature",svix_signature)
     
             if (!svix_id || !svix_timestamp || !svix_signature || Array.isArray(svix_id) || Array.isArray(svix_timestamp) || Array.isArray(svix_signature)) {
                 return ctx.badRequest('Faltan encabezados de Svix o el formato es inválido');
@@ -38,15 +32,11 @@ export default factories.createCoreController('api::signup.signup',({strapi})=>(
                         "svix-signature": svix_signature,
                     }
                 )
-                console.log("evt",evt)
             } catch (error) {
-                console.log("error evt",error)
                 return ctx.badRequest('Firma de Webhook inválida')
             }
     
             const { id, image_url, email_addresses, first_name, last_name, external_accounts } = evt.data
-            
-            console.log("data webhook", id, image_url, email_addresses, first_name, last_name, external_accounts )
             
             const provider = external_accounts.length && external_accounts[0].provider 
             const email = email_addresses[0].email_address
@@ -62,23 +52,21 @@ export default factories.createCoreController('api::signup.signup',({strapi})=>(
                     throw new Error("No se encontró el rol 'authenticated' en Strapi");
                 }
                 
-                const user = await strapi
-                .plugin("users-permissions")
-                .service("user")
-                .add({
-                    name:first_name,
-                    lastname:last_name,
-                    username:userName,
-                    email:email,
-                    clerkId:id,
-                    role:role.id,
-                    confirmed:true,
-                    provider:provider,
-                    avatar_url:image_url
-                })
-    
-                console.log("create user",user)
-                
+                await strapi
+                    .plugin("users-permissions")
+                    .service("user")
+                    .add({
+                        name:first_name,
+                        lastname:last_name,
+                        username:userName,
+                        email:email,
+                        clerkId:id,
+                        role:role.id,
+                        confirmed:true,
+                        provider:provider,
+                        avatar_url:image_url
+                    })
+                    
                 return ctx.send({ message: 'Usuario creado' });
             }else{
                 console.log("método no autorizado")
