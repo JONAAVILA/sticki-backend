29127de5f760c382fd085e7b4202944a0f93dc8a
sidonay499 <jona_1969@hotmail.com>
2026-04-22 10:10:53 -0300

    update header

--- src/middlewares/auth-clerk.ts
+++ src/middlewares/auth-clerk.ts
@@ -13,7 +13,7 @@ export default (config, { strapi }: { strapi: Core.Strapi }) => {
       if(ctx.path.startsWith('/admin/')) return await next()
         
       strapi.log.info('In auth-clerk middleware.')
-      const header = ctx.get('Authorization');
+      const header = ctx.request.header['authorization']
       const authHeader = header.authorization
       console.log("Authheaders auth clerk",authHeader)
       console.log("header auth clerk",ctx.request.header)
