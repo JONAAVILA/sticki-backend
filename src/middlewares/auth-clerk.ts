5df1815137ba207a6dd666d3e65934a76c9474ae
sidonay499 <jona_1969@hotmail.com>
2026-04-22 10:57:04 -0300

    add console log

--- src/middlewares/auth-clerk.ts
+++ src/middlewares/auth-clerk.ts
@@ -9,17 +9,20 @@ const { CLERK_SECRET_KEY } = process.env
 export default (config, { strapi }: { strapi: Core.Strapi }) => {
   return async (ctx, next) => {
     try {
+      console.log("path",ctx.path)
       if(ctx.path === '/auth/signup-webhook') return await next()
       if(ctx.path.startsWith('/admin/')) return await next()
-        
+      console.log("headers",ctx.headers)
       strapi.log.info('In auth-clerk middleware.')
       const authHeader = ctx.request.headers['authorization']
+      console.log("authHeader",authHeader)
 
       if (!authHeader){
         return ctx.unauthorized("Invalid auth header")
       }
 
       const token = authHeader.split(' ')[1]
+      console.log("token",token)
       
       const session = await verifyToken(
           token,
