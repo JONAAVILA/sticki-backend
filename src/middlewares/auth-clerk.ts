34bcc348ae009b7fd0bd7d6c45e3ea94bbe84f41
sidonay499 <jona_1969@hotmail.com>
2026-04-22 11:17:39 -0300

    update path

--- src/middlewares/auth-clerk.ts
+++ src/middlewares/auth-clerk.ts
@@ -10,8 +10,8 @@ export default (config, { strapi }: { strapi: Core.Strapi }) => {
   return async (ctx, next) => {
     try {
       console.log("path",ctx.path)
-      if(ctx.path === 'api/auth/signup-webhook') return await next()
-      if(ctx.path.startsWith('api/admin/')) return await next()
+      if(ctx.path === '/api/auth/signup-webhook') return await next()
+      if(ctx.path.startsWith('/api/admin/')) return await next()
       console.log("headers",ctx.headers)
       strapi.log.info('In auth-clerk middleware.')
       const authHeader = ctx.request.headers['authorization']
